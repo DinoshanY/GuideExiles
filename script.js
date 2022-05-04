7c80fd30b23c7fd5269d47ca7d5e10e857912716
DinoshanY <dinoshan0710@gmail.com>
2022-05-04 11:52:42 +0200

    added jquery and fixed a navbar nug

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
-const navLinks = document.querySelectorAll(".nav-item");
-const menuToggle = document.getElementById("navbarSupportedContent");
-const bsCollapse = new bootstrap.Collapse(menuToggle);
-
-
-
-
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+const bsCollapse = new bootstrap.Collapse(menuToggle)
 navLinks.forEach((l) => {
-  l.addEventListener("click", closeOnClick());
-});
-
-
-function closeOnClick() {
-  if (window.innerWidth < 1400) {
-    navLinks.forEach((l) => {
-      l.addEventListener("click", () => {
-        bsCollapse.toggle();
+    l.addEventListener('click', () => { bsCollapse.toggle(),$(function () {
+      $(document).scroll(function () {
+        var $nav = $(".navbar-sticky-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
       });
-    });
-  } else {
-}
-}
-
-
-
+    }); })
+})
